033905848ce36a23a2ec4fd8a08f654bd0e4165b
Roger Mai <cab022010@coderacademy.edu.au>
2020-11-09 22:58:00 +1000

    usernamepassword fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def new
     @user = User.new 
   end
 
   def create
-    User.create!(
-      username: params[:user][:username],
-      passwork: params[:user][:password]
-    )
-    
+    User.create!(user_params)
   end
+
+  private
+
+  def user_params
+    return params.require(:user).permit(:username, :password)
+  end
+
 end
